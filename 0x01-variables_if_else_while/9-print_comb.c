7a3d71e2c331e8e9276a4c040c223b60093373b0
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2023-04-22 06:13:51 -0700

    print-comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -Print the combinations of single-digital numbers
+ *
+ * Return: Alwats 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
