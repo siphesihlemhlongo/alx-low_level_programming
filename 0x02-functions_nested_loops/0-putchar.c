5f7db5e42f39b26cf08dccb90db4d4035c19281c
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2023-04-25 22:58:06 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,15 @@
-#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 /**
- * main -Entry point
+ * main - Entry point
+ *
  * Return: Always 0
  */
-int main(void)
 
+int main(void)
 {
-	_putchar(' ');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - make a void that print alphabet
+ * Return: void
+ */
+void print_alphabet(void);
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	-putchar('\n');
+}
