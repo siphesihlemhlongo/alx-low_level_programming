9cbf37d7c686f44c10dccad675d7177c1a1e120f
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2023-05-02 13:43:31 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -14,7 +14,7 @@ nb = _atoi("98");
 printf("%d\n", nb);
 nb = _atoi("-402");
 printf("%d\n", nb);
-nb = _atoi("          ------++++++-----+++++--98");
+nb = _atoi("          ------++++++-----+++++- -98");
 printf("%d\n", nb);
 nb = _atoi("214748364");
 printf("%d\n", nb);
